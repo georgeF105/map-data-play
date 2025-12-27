13d501c36182cedae03c35e03adbb193d986cab7
George Francis <georgefrancis105@gmail.com>
2025-12-27 13:53:52 +1300

    Add water quality layer

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import AppMap from "./components/AppMap/AppMap";
 import LocalGeoJson from "./components/LocalGeoJson/LocalGeoJson";
+import WaterQualityLayer from "./components/WaterQualityLayer/WaterQualityLayer";
 
 function App() {
   return (
     <>
       <AppMap>
         <LocalGeoJson />
+        <WaterQualityLayer />
       </AppMap>
     </>
   );
